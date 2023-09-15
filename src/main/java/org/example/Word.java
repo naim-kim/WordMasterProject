ceeb0a37b06c9324cc2a8acbe2a7da9ee5895939
naim-kim <143364203+naim-kim@users.noreply.github.com>
2023-09-15 13:16:43 +0900

    minor fix

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -6,35 +6,46 @@ public class Word {
     private int level;
     private String word;
     private String meaning;
-Word() {}
-    Word(int id, int level, String word, String meaning){
-        this.id=id;
-        this.level=level;
-        this.word=word;
-        this.meaning=meaning;
+
+    Word() {
+    }
+
+    Word(int id, int level, String word, String meaning) {
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
 
     }
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public int getLevel() {
         return level;
     }
+
     public void setLevel(int level) {
         this.level = level;
     }
+
     public String getWord() {
         return word;
     }
+
     public void setWord(String word) {
         this.word = word;
     }
+
     public String getMeaning() {
         return meaning;
     }
+
     public void setMeaning(String meaning) {
         this.meaning = meaning;
     }
@@ -42,13 +53,14 @@ Word() {}
     @Override
     public String toString() {
 
-    String slevel = "";
-    for(int i = 0; i < level; i++) slevel += "*";
-    String str = String.format("%-3s", slevel)
-            + String.format("%15s", word) + " " + meaning;
-    return str;
+        String slevel = "";
+        for (int i = 0; i < level; i++) slevel += "*";
+        String str = String.format("%-3s", slevel)
+                + String.format("%15s", word) + " " + meaning;
+        return str;
     }
-    public String toFileString(){
+
+    public String toFileString() {
         return this.level + "|" + this.word + "|" + this.meaning;
     }
 }
